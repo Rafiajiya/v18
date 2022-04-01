35fc65410a28499ae3e33a188059907ef147adc1
Rafiajiya <94803405+Rafiajiya@users.noreply.github.com>
2022-04-02 06:56:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ gc1 = ''
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL', ''] // ganti jadi group lu
-global.owner = ['6288279268363'] // Put your number here //owner eval
-global.kontak = ['6288279268363','0'] //Ketika ada yang ngetik #owner
-global.mods = ['6288279268363'] // Want some help?
-global.prems = ['6288279268363'] // Premium user has unlimited limit
+global.owner = ['62882007331744'] // Put your number here //owner eval
+global.kontak = ['6288233481992','0'] //Ketika ada yang ngetik #owner
+global.mods = ['62882007331744'] // Want some help?
+global.prems = ['6288233481992'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -57,20 +57,20 @@ bearkey = 'buy'
 namaig = 'https://instagram.com/drak_ipul123'
 namagithub = 'GAK ADA :)'
 kasihcaption = `Nih kak`
-namakontak1 = 'King Of Bear'
-namakontak2 = 'King Of Bear'
+namakontak1 = 'Rafi aji'
+namakontak2 = 'Rafi Bot'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'By King Of Bear' // ganti aja
-global.author = 'King Of Bear' // ganti aja
+global.packname = 'By Rafi Aji' // ganti aja
+global.author = 'Rafi Aji' // ganti aja
 
 //yyy
 bc = 'King Of Bear' //King Of Bear Broadcast
 footer = '\n©King Of Bear'
-namabot = 'YUKI BOT'
-namalu = 'King Of Bear'
+namabot = 'Rafi Botz'
+namalu = 'Rafi aji'
 
 
 // 
